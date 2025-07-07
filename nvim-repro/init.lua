d211edec1b9fffead495902276c1ac7087905c61
Loi Chyan <loichyan@foxmail.com>
2025-07-07 15:56:58 +0800

    [TEST] add a minimal reproducing configuration

---
+++ nvim-repro/init.lua
@@ -0,0 +1,23 @@
+-- Clone latest 'mini.nvim' (requires Git CLI installed)
+vim.cmd('echo "Installing `mini.nvim`" | redraw')
+local mini_path = vim.fn.stdpath('data') .. '/site/pack/deps/start/mini.nvim'
+local clone_cmd = { 'git', 'clone', '--depth=1', 'https://github.com/echasnovski/mini.nvim', mini_path }
+vim.fn.system(clone_cmd)
+vim.cmd('echo "`mini.nvim` is installed" | redraw')
+
+-- Make sure 'mini.nvim' is available
+vim.cmd('packadd mini.nvim')
+require('mini.deps').setup()
+
+-- Add extra setup steps needed to reproduce the behavior
+-- Use `MiniDeps.add('user/repo')` to install another plugin from GitHub
+
+require('mini.completion').setup()
+vim.o.signcolumn = 'yes'
+
+-- https://pkg.go.dev/golang.org/x/tools/gopls
+vim.lsp.config('gopls', {
+  cmd = { 'gopls' },
+  filetypes = { 'go' },
+})
+vim.lsp.enable('gopls')
---
+++ nvim-repro/test.go
@@ -0,0 +1,11 @@
+package main
+
+func myfunc()       {}
+func myfunc2(a int) {}
+
+func main() {
+	// Press <CR> to accept the selection
+	// But myfunc() will follow a newline, since snippet not expanded
+
+	// While myfunc2() will actually not follow a neline
+}
