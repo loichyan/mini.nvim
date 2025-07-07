38997e4baba12beb2b8f8239e453e586bde4aebc
Loi Chyan <loichyan@foxmail.com>
2025-07-07 15:56:58 +0800

    [TEST] add a minimal reproducing configuration

---
+++ nvim-repro/test.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+	test := "string string"
+	_ = test
+	// 1) type: test.spli
+	//               ^^ corresponds to how many 'x' will be prepended
+	// 2) select: split!
+	// 3) confirm with any character
+}
